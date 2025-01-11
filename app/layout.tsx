d4adba25e5df05f72606bd9b64a0ae8f4ed6e730
acetbug <1677098687@qq.com>
2025-01-11 23:57:58 +0800

    fix: 移除未使用的Metadata导入

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import type { Metadata } from "&/next";
 import { Geist, Geist_Mono } from "&/next/font/google";
 import HomeIcon from "@/components/homeIcon";
 import "./globals.css";
